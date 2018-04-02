2126b1793e5d8b796809aed1d764c555c14a8553
Ally Netzel <netzelally@gmail.com>
2018-04-02 01:06:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(nums)
+  nums.sort 
+end
 
+def sort_array_desc(nums)
+  nums.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort{|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array.sort{|x, y| x[1] <=> y[2] }
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array) 
+  array.each do |name|
+    name[2] = "$"
+    end
+  array
+end
+
+def find_a(array)
+  array.select {|str| str.start_with?('a')}
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |item|
+    sum += item
+  end
+  return sum
+end
+  
+def add_s(array)
+  array.each_with_index.collect{|element, index|  element << "s" }
+end
