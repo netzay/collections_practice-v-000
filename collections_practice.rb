631f4a18b2e91dc0c6ec55ed46914f6c443fa317
Ally Netzel <netzelally@gmail.com>
2018-04-02 01:23:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,5 +38,4 @@ def sum_array(array)
 end
   
 def add_s(array)
-  array.each_with_index.collect{|element, index|  element << "s" unless index == 1 element << array[1]}
-end
+  array.each_with_index.collect{|element, index|  element << "s" unless index == 1 
