e3d6014a78e39948fa9ab8260cc3bffa00e63340
Ally Netzel <netzelally@gmail.com>
2018-04-02 01:21:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,6 +38,6 @@ def sum_array(array)
 end
   
 def add_s(array)
-  array.each_with_index.collect{|element, index|  element << "s"}
-    element << array[1]
+  array.each_with_index.collect{|element, index|  element << "s" element << array[1]}
+    
 end
